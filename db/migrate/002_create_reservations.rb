0a4de2310979e0627d88eb3b6e06861e62001cd5
Jackmt9 <jackmt9@gmail.com>
2020-03-23 12:12:25 -0400

    Outline files

---
+++ db/migrate/002_create_reservations.rb
@@ -0,0 +1,11 @@
+class CreateReservations < ActiveRecord::Migration[5.2]
+    def change
+        create_table :reservations do |t|
+            t.integer :restaurant_id
+            t.integer :user_id
+            t.integer :table_size
+            t.date :date
+            t.time :time
+        end
+    end
+end
